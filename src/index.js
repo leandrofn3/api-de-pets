e2935232a148916f450714762175f1d495c362db
leandrofn3 <lf1938833@gmail.com>
2026-03-14 11:07:09 -0300

    Criando a as configurações do projeto as variaveis de ambiente e o instalando o cors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Jesus e o meu senhor!")
+import express from "express";
+import * as dotenv from "dotenv";
+import { pets } from "./data.js";
+import {randomUUID} from "crypto";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    console.info("Bem-vindo a API de Pets!");
+
+    res.status(200).send({
+        ok: true,
+        messagem: "Rota execultada com sucesso!"
+    });
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.info(`O servidor está execultando na porta: ${port}!`);
+});
