8806a96f8e9b8c3a0f80e8b0ba883cad190822c1
leandrofn3 <lf1938833@gmail.com>
2026-03-15 17:41:38 -0300

    criação da rota post para criar um novo pet, com a criação de middlewares para validar os campos.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { pets } from "./data.js";
-import {randomUUID} from "crypto";
+import { randomUUID } from "crypto";
 import cors from "cors";
+import { validateFields } from "./middlewares.js";
 
 dotenv.config();
 
@@ -17,6 +18,34 @@ app.get("/", (req, res) => {
         ok: true,
         messagem: "Rota execultada com sucesso!"
     });
+});
+
+app.post("/pets", [validateFields], (req, res) => {
+    try {
+        const { name, race, age, tutorsName } = req.body;
+
+        const newPet = {
+            id: randomUUID(),
+            name: name.trim(),
+            race: race.trim(),
+            age: Number(age),
+            tutorsName: tutorsName.trim()
+        };
+
+        pets.push(newPet);
+
+        return res.status(201).json({
+            ok: true,
+            message: "pet created successfully",
+            data: newPet
+        });
+
+    } catch (error) {
+        return res.send(500).json({
+            ok: false,
+            message: error.toString()
+        });
+    };
 });
 
 const port = process.env.PORT;
---
+++ src/middlewares.js
@@ -0,0 +1,36 @@
+export const validateFields = (req, res, next) => {
+    try {
+        const { name, race, age, tutorsName } = req.body;
+        const errors = [];
+
+        if (!name) errors.push({ ok: false, message: "The name field is required!" });
+        if (!race) errors.push({ ok: false, message: "The race field is mandatory!" });
+        if (!age) errors.push({ ok: false, message: "The age field is required!" });
+        if (!tutorsName) errors.push({ ok: false, message: "The tutor's name field is required!" });
+
+        if (name === "") errors.push({ ok: false, message: "The name field cannot be empty!" });
+        if (race === "") errors.push({ ok: false, message: "The field can't be left empty!" });
+        if (age === "") errors.push({ ok: false, message: "The age field cannot be left blank!" });
+        if (tutorsName === "") errors.push({ ok: false, message: "The tutor's name field cannot be empty!" });
+
+        if (typeof name !== "string") errors.push({ ok: false, message: "The name field must be of type string!" });
+        if (typeof race !== "string") errors.push({ ok: false, message: "The race field must be of type string!" });
+        if (typeof age !== "number" || age < 0) errors.push({ ok: false, message: "The age field must be a number and greater than zero!" });
+        if (typeof tutorsName !== "string") errors.push({ ok: false, message: "The tutor's name field must be of type string!" });
+
+        if (errors.length > 0) {
+            return res.status(400).json({
+                ok: false,
+                message: "invalid request",
+                reason: errors
+            });
+        };
+
+        return next()
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            message: error.toString()
+        });
+    }
+};
